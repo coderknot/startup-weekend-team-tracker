f0b5e6c37c4c9b20e7791bb0d6fde61150adffb3
CoderKnot <coder.knot@gmail.com>
2017-03-24 11:17:00 -0700

    create index.vtl and add root route pointing to index

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,11 @@ public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
 
-    
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("teams", Team.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
