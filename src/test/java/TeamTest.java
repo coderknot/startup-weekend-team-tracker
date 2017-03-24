b3343395f7090e01dd8c96afc6d6ca60ad748fe0
CoderKnot <coder.knot@gmail.com>
2017-03-24 09:21:52 -0700

    create Team.java and test that Team objects instantiate correctly

---
+++ src/test/java/TeamTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TeamTest {
+
+  @Test
+  public void Team_instantiatesCorrectly_true() {
+    Team testTeam = new Team("Team TOT");
+    assertTrue(testTeam instanceof Team);
+  }
+}
