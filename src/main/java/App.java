3af302e31c7235902c03f58ba035ff695b06dffb
CoderKnot <coder.knot@gmail.com>
2017-03-25 18:36:11 -0700

    create app.css and test styles with spark application

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
@@ -61,5 +62,5 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
-  
+
 }
